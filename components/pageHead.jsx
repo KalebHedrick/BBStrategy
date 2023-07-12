fbadcbc78973a2a07b2e295c8603c976c4685cba
KalebHedrick <destroycows@gmail.com>
2023-07-12 16:56:44 -0400

    fix vite

---
+++ components/pageHead.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+const maps = ["map1","map2","map3"]
+function Header() {
+  
+ 
+  return (<div backgroundColor={"#ABCDDD"} style = {{backgroundColor: "red", flex: 1}}> 
+<text>hiddddddddddddddddddddddddddddddddddd</text>
+    </div>
+  )
+}
+
+export default Header

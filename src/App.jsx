10e5a56051363fbc2329954409330f052d763e66
KalebHedrick <destroycows@gmail.com>
2023-07-12 16:02:59 -0400

    canvas draw

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import CanvasDraw from 'react-canvas-draw'
+const maps = ["map1","map2","map3"]
 function App() {
   const [count, setCount] = useState(0)
-
+  const [map, setMap] = useState(maps[0])
   return (
     <>
+    <CanvasDraw />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />

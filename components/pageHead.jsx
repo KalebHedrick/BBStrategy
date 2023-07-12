9a8e1328b9fd82ee19965d5d79a3744662769282
KalebHedrick <destroycows@gmail.com>
2023-07-12 17:03:35 -0400

    viteIssue

--- components/pageHead.jsx
+++ components/pageHead.jsx
@@ -3,8 +3,8 @@ const maps = ["map1","map2","map3"]
 function Header() {
   
  
-  return (<div backgroundColor={"#ABCDDD"} style = {{backgroundColor: "red", flex: 1}}> 
-<text>hiddddddddddddddddddddddddddddddddddd</text>
+  return (<div style={{backgroundColor:"red", height:"100%"}}> 
+
     </div>
   )
 }
